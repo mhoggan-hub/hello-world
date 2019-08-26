6896b55ffc776a37ac9ab3c4268a8d2c4c00ede4
mhoggan-hub <mhoggan@mymail.mines.edu>
2019-08-25 19:36:07 -0600

    I finished assignment A1

---
+++ A1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //This smiley face is based off of tayyabr@ifi.uio.no on loveascii.com
+    cout << "              OOOOOOOOOOO         " << endl;
+    cout << "          OOOOOOOOOOOOOOOOOOO         " << endl;
+    cout << "       OOOOOO  OOOOOOOOO  OOOOOO       " << endl;
+    cout << "     OOOOOO      OOOOO      OOOOOO     " << endl;
+    cout << "   OOOOOOOO  #   OOOOO  #   OOOOOOOO         " << endl;
+    cout << "  OOOOOOOOOO    OOOOOOO    OOOOOOOOOO       " << endl;
+    cout << " OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO          " << endl;
+    cout << " OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO      " << endl;
+    cout << " OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO      " << endl;
+    cout << "  OOOO   OOOOOOOOOOOOOOOOOOOOO   OOOO     " << endl;
+    cout << "    OOOO  OOOOOOOOOOOOOOOOOOO   OOOO       " << endl;
+    cout << "      OOOO    OOOOOOOOOOO     OOOO       " << endl;
+    cout << "         OOOO              OOOO        " << endl;
+    cout << "           OOOOOOOOOOOOOOOOOO   " << endl;
+    cout << "              OOOOOOOOOOOO   " << endl;
+
+    const int myAge= 19; //These are three unchangeable variables that I can use throughout the program
+    const int hoursICanProbablyWalkAMile= 2; //Usage of camel case
+    const int sticksOfBlackLicoriceEverEaten= 1;
+
+            cout << "I am "<< myAge << " years old."  <<endl;
+            cout << "I can probably walk a mile in less than " << hoursICanProbablyWalkAMile << " hours." << endl;
+            cout << "I have eaten " <<sticksOfBlackLicoriceEverEaten<< " stick of black licorice in my life." <<endl;
+
+
+
+    return 0;
+}
